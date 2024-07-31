b35e6e934651657a5c9ac32cd19f182ee3b63c88
gaganjotshan <gagan.shan7@gmail.com>
2024-07-31 15:00:21 +0200

    Troubleshooting

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,8 +1,11 @@
-import os
 import sys
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 class PredictPipeline:
@@ -11,10 +14,11 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            model_path=os.path.join("artifacts","model.pkl")
-            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            model_path='artifacts/model.pkl'
+            preprocessor_path='artifacts/proprocessor.pkl'
             print("Before Loading")
             model=load_object(file_path=model_path)
+            
             preprocessor=load_object(file_path=preprocessor_path)
             print("After Loading")
             data_scaled=preprocessor.transform(features)
